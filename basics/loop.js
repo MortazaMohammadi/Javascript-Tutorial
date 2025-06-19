bf24822357dc7e12928ce29757f71dc5f1cdb6b2
MortazaMohammadi <mortazamohammadi800@gmail.com>
2025-06-19 19:43:45 +0430

    update loop

---
+++ basics/loop.js
@@ -0,0 +1,20 @@
+let a = 50
+ret = true
+while(ret){
+    if (a <= 0){
+        ret = false
+    }
+    console.log(a)
+    a -= 1
+}
+a = 50
+while (a !== 0){
+    console.log(a)
+    a -=1
+}
+
+a = 5
+do {
+    console.log(a)
+    a -= 1
+}while(a == 0)
